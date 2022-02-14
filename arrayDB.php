1cb1837b0b1c5a6b848771be4aa550a96987ac12
manuflorido <60346060+manuflorido@users.noreply.github.com>
2022-02-14 10:05:43 +0100

    Creado reserva y añadidos los array nuevos

--- arrayDB.php
+++ arrayDB.php
@@ -10,7 +10,13 @@ $vueloDestino=array(
     array("Avion" => "Iberia-WH44","Destino" => "Roma" ),
     array("Avion" => "Ryanair-EL00","Destino" => "Madrid" ),
     array("Avion" => "Vueling-AE12","Destino" => "Roma" ),
-    array("Avion" => "Volotea-EA21","Destino" => "Roma" )
+    array("Avion" => "Volotea-EA21","Destino" => "Milán" ),
+    array("Avion" => "Iberia-WE44","Destino" => "New York" ),
+    array("Avion" => "Ryanair-EW44","Destino" => "Tokio" ),
+    array("Avion" => "Vueling-ZK87","Destino" => "Chicago" ),
+    array("Avion" => "Volotea-UA21","Destino" => "Paris" ),
+    array("Avion" => "Iberia-WH44","Destino" => "Oporto" ),
+    array("Avion" => "Ryanair-EL00","Destino" => "Los Ángeles" )
 );
 
 $vueloFabricante=array(
@@ -75,4 +81,57 @@ $vueloPasajeros=array(
 );
 
 //TRABAJO FINALIZADO 28/01/2022 POR MANUEL FLORIDO
+
+$vueloHoras=array(
+    array("Avion" => "Vueling-AE12","Hora" => "15/03/2022 12:00:00" ),
+    array("Avion" => "Volotea-EA21","Hora" => "16/02/2022 16:30:20" ),
+    array("Avion" => "Iberia-WE44","Hora" => "16/02/2022 15:45:00" ),
+    array("Avion" => "Ryanair-EW44","Hora" => "17/02/2022 18:11:40" ),
+    array("Avion" => "Vueling-ZK87","Hora" => "17/02/2022 12:50:00" ),
+    array("Avion" => "Volotea-UA21","Hora" => "18/02/2022 10:45:30" ),
+    array("Avion" => "Iberia-WH44","Hora" => "19/02/2022 11:20:00" ),
+    array("Avion" => "Ryanair-EL00","Hora" => "20/02/2022 13:57:00" )
+);
+
+$vueloDivisa=array(
+    array("Divisa" => "Vueling-AE12","Valor" => "0,007576" ),
+    array("Divisa" => "Volotea-EA21","Valor" => "0,877732" ),
+    array("Divisa" => "Iberia-WE44","Valor" => "0,24" ),
+    array("Divisa" => "Ryanair-EW44","Valor" => "0,065" )
+);
+
+$vueloAeropuerto=array(
+    array("Ciudad" => "Milán","Aeropuerto" => "Aeropuerto Milán-Malpensa" ),
+    array("Ciudad" => "Roma","Aeropuerto" => "Aeropuerto Roma-Fiumicino" ),
+    array("Ciudad" => "Estambul","Aeropuerto" => "Aeropuerto de Estambul" ),
+    array("Ciudad" => "Madrid","Aeropuerto" => "Aeropuerto Adolfo Suárez Madrid-Barajas" ),
+    array("Ciudad" => "Lima","Aeropuerto" => "Aeropuerto Internacional Jorge Chávez" ),
+    array("Ciudad" => "Bilbao","Aeropuerto" => "Aeropuerto Internacional Bilbao" ),
+    array("Ciudad" => "New York","Aeropuerto" => "Aeropuerto Internacional John F. Kennedy" ),
+    array("Ciudad" => "Tokio","Aeropuerto" => "Aeropuerto Internacional de Haneda" ),
+    array("Ciudad" => "Chicago","Aeropuerto" => "Aeropuerto Internacional O'Hare" ),
+    array("Ciudad" => "Paris","Aeropuerto" => "Aeropuerto Internacional de Paris-Charles de Gaulle" ),
+    array("Ciudad" => "Oporto","Aeropuerto" => "Aeropuerto Francisco Sá Carneiro" ),
+    array("Ciudad" => "Los Ángeles","Aeropuerto" => "Aeropuerto Internacional de Los Ángeles" )
+);
+
+$vueloPrecio=array(
+    array("Avion" => "Vueling-AE12","Precio" => "120","Destino" => "Milán" ),
+    array("Avion" => "Volotea-EA21","Precio" => "90","Destino" => "Roma" ),
+    array("Avion" => "Iberia-WE44","Precio" => "200","Destino" => "Estambul" ),
+    array("Avion" => "Ryanair-EW44","Precio" => "50","Destino" => "Madrid" ),
+    array("Avion" => "Vueling-ZK87","Precio" => "1000","Destino" => "Lima" ),
+    array("Avion" => "Volotea-UA21","Precio" => "70","Destino" => "Bilbao" ),
+    array("Avion" => "Iberia-WH44","Precio" => "95","Destino" => "Roma" ),
+    array("Avion" => "Ryanair-EL00","Precio" => "80","Destino" => "Madrid" ),
+    array("Avion" => "Vueling-AE12","Precio" => "65","Destino" => "Roma" ),
+    array("Avion" => "Volotea-EA21","Precio" => "77","Destino" => "Milán" ),
+    array("Avion" => "Iberia-WE44","Precio" => "1200","Destino" => "New York" ),
+    array("Avion" => "Ryanair-EW44","Precio" => "1100","Destino" => "Tokio" ),
+    array("Avion" => "Vueling-ZK87","Precio" => "1500","Destino" => "Chicago" ),
+    array("Avion" => "Volotea-UA21","Precio" => "200","Destino" => "París" ),
+    array("Avion" => "Iberia-WH44","Precio" => "45","Destino" => "Oporto" ),
+    array("Avion" => "Ryanair-EL00","Precio" => "1700","Destino" => "Los Ángeles" )
+);
+
 ?>
